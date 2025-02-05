2aff35130d5d89bc94aef0817dfdb1f07746c4bc
bautiexposito <bautistaexposito@outlook.com>
2025-02-05 12:15:03 -0300

    creacion de vista

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,100 +1,88 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-		text-decoration: none;
-	}
-
-	a:hover {
-		color: #97310e;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-		min-height: 96px;
-	}
-
-	p {
-		margin: 0 0 10px;
-		padding:0;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>CRUD CODEIGNITER</title>
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	</head>
 <body>
+	<div class="container">
+		<!-- Titulo -->
+		<div class="row">
+			<h2>CRUD CODEIGNITER</h2>
+		</div>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
+		<!-- Formulario -->
+		 <div class="mb-5">
+			<form action="">
+				<div class="row">
+					<div class="form-group col-sm-4">
+						<label for="">Nombre</label>
+						<input type="text" name="nombre" class="form-control" required placeholder="Nombre">
+					</div>
+					<div class="form-group col-sm-4">
+						<label for="">Apellido paterno</label>
+						<input type="text" name="ap" class="form-control" required placeholder="Apellido paterno">
+					</div>
+					<div class="form-group col-sm-4">
+						<label for="">Apellido materno</label>
+						<input type="text" name="am" class="form-control" required placeholder="Apellido materno">
+					</div>
+				</div>
+				<div class="row">
+					<div class="form-group col-sm-4">
+						<label for="">Fecha de nacimiento</label>
+						<input type="date" name="fn" class="form-control" required>
+					</div>
+					<div class="form-group col-sm-4">
+						<label for="">Sexo</label>
+						<input type="text" name="genero" class="form-control" required placeholder="M o F">
+					</div>
+				</div>
+				<button type="submit" class="btn btn-primary btn-block">Guardar</button>
+			</form>
+		 </div>
+		 
+		<!-- Tabla de datos -->
+		 <div class="row">
+			<div class="card col-12">
+				<div class="card-header">
+					<h4>Tabla de personas</h4>
+				</div>
+				<div class="card-body">
+					<table class="table table-striped">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">Nombre</th>
+								<th scope="col">Fecha de nacimiento</th>
+								<th scope="col">Genero</th>
+								<th scope="col">Editar</th>
+								<th scope="col">Eliminar</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php 
+								$count = 0;
+								foreach ($personas as $persona) {
+									echo '
+										<tr>
+											<td>'.++$count.'</td>
+											<td>'.$persona->nombre.' '.$persona->ap.' '.$persona->am.'</td>
+											<td>'.$persona->fn.'</td>
+											<td>'.$persona->genero.'</td>
+											<td><button type="button" class="btn btn-warning text-white" onclick="llenar_datos('.$persona->id.', `'.$persona->nombre.'`, `'.$persona->ap.'`, `'.$persona->am.'`, `'.$persona->fn.'`, `'.$persona->genero.'`)">Editar</button></td>
+											<td><a href="'.base_url('welcome/eliminar/'.$persona->id).'" type="button" class="btn btn-danger">Eliminar</a></td>
+										</tr>
+									';
+								}
+							?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		 </div>
 	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
 </body>
 </html>
