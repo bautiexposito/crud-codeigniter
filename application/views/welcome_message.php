928e0d928cdbbc25273cc9e80b10a22bf9ad897a
bautiexposito <bautistaexposito@outlook.com>
2025-02-18 18:38:28 -0300

    welcome message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -23,24 +23,24 @@
 					</div>
 					<div class="form-group col-sm-4">
 						<label for="">Apellido paterno</label>
-						<input type="text" name="ap" class="form-control" required placeholder="Apellido paterno">
+						<input type="text" name="apellido_paterno" class="form-control" required placeholder="Apellido paterno">
 					</div>
 					<div class="form-group col-sm-4">
 						<label for="">Apellido materno</label>
-						<input type="text" name="am" class="form-control" required placeholder="Apellido materno">
+						<input type="text" name="apellido_materno" class="form-control" required placeholder="Apellido materno">
 					</div>
 				</div>
 				<div class="row">
 					<div class="form-group col-sm-4">
 						<label for="">Fecha de nacimiento</label>
-						<input type="date" name="fn" class="form-control" required>
+						<input type="date" name="fecha_nacimiento" class="form-control" required>
 					</div>
 					<div class="form-group col-sm-4">
 						<label for="">Sexo</label>
-						<input type="text" name="genero" class="form-control" required placeholder="M o F">
+						<input type="text" name="sexo" class="form-control" required placeholder="M o F">
 					</div>
 				</div>
-				<button type="submit" class="btn btn-primary btn-block">Guardar</button>
+				<button type="submit" class="btn btn-primary w-100">Guardar</button>
 			</form>
 		 </div>
 		 
@@ -58,31 +58,27 @@
 								<th scope="col">Nombre</th>
 								<th scope="col">Fecha de nacimiento</th>
 								<th scope="col">Genero</th>
-								<th scope="col">Editar</th>
-								<th scope="col">Eliminar</th>
 							</tr>
 						</thead>
 						<tbody>
-							<?php 
-								$count = 0;
-								foreach ($personas as $persona) {
-									echo '
-										<tr>
-											<td>'.++$count.'</td>
-											<td>'.$persona->nombre.' '.$persona->ap.' '.$persona->am.'</td>
-											<td>'.$persona->fn.'</td>
-											<td>'.$persona->genero.'</td>
-											<td><button type="button" class="btn btn-warning text-white" onclick="llenar_datos('.$persona->id.', `'.$persona->nombre.'`, `'.$persona->ap.'`, `'.$persona->am.'`, `'.$persona->fn.'`, `'.$persona->genero.'`)">Editar</button></td>
-											<td><a href="'.base_url('welcome/eliminar/'.$persona->id).'" type="button" class="btn btn-danger">Eliminar</a></td>
-										</tr>
-									';
-								}
-							?>
+							<tr>
+								<th scope="row">1</th>
+								<td>Bautista</td>
+								<td>06/11/2000</td>
+								<td>Masculino</td>
+							</tr>
+							<tr>
+								<th scope="row">2</th>
+								<td>Felipe</td>
+								<td>07/10/2004</td>
+								<td>Masculino</td>
+							</tr>
 						</tbody>
 					</table>
 				</div>
 			</div>
 		 </div>
+
 	</div>
 </body>
 </html>
